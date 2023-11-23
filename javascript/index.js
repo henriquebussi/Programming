2c916390d862b2fc5b2c25ecc242b9b3303aaeec
henriquebussi <henriquebussidossantos@gmail.com>
2023-11-23 15:06:14 -0300

    js

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+// Tipos de Variaveis
+
+var henrique = 'henrique' //declara uma variavel
+let bussi = 'bussi'
+const santos = 'santos'
+
+// Variáveis declaradas são criadas antes de qualquer código ser executado. As variáveis não declaradas não existem até quando o código atribuir um valor a ela.
+
+console.log(henrique, bussi, santos);
