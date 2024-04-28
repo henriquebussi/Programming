2ae9d8b04e6a9f3d5f195a8c309f3ef3b5c55b1b
henriquebussi <henriquebussidossantos@gmail.com>
2024-04-28 16:42:13 -0300

    c++math

--- c++/code++.cpp
+++ c++/code++.cpp
@@ -1,65 +1,65 @@
 #include <iostream>
 // Biblioteca que tem muita coisa(principalmente Output and Input, std)
 
-
-namespace one{ 
+namespace one
+{
     // namespace são luagres onde variavis são armazenadas, podem ter nomes iguais mas com valores diferentes da função originaria
     int age = 1;
 }
 
-int helloworld(){
-        /*
-    std - Standart
-    cout - C-out, character output ou Output de Characteres
-    << - Output
-    */
-//BASIC   
+int helloworld()
+{
+    /*
+std - Standart
+cout - C-out, character output ou Output de Characteres
+<< - Output
+*/
+    // BASIC
     std::cout << "hello world" << '\n';
     std::cout << "Vai se foder c++" << '\n';
 
-//VARIABLES
-    int age = 16; // int - variavel de inteiros (Z)
-    double pi = 3.14; // double - variavel dos Racionais (R)
-    char initial = 'H'; // char - apenas um character
-    bool power = true; // boolean - True/False
-    std::string name = "Henrique"; // std::string - Texto 
+    // VARIABLES
+    int age = 16;                  // int - variavel de inteiros (Z)
+    double pi = 3.14;              // double - variavel dos Racionais (R)
+    char initial = 'H';            // char - apenas um character
+    bool power = true;             // boolean - True/False
+    std::string name = "Henrique"; // std::string - Texto
 
-    std::cout << age << " anos" << '\n'; 
+    std::cout << age << " anos" << '\n';
     std::cout << "hello " << name << '\n';
 
-// MATH
+    // BASIC MATH
     const double DESCONTO = 0.10; // const - não pode ser mais modificada, "read-only"
-    double price = 50;
-    double finalprice = price-price*DESCONTO;
-
+    int price = 50;
+    double finalprice = price - price * DESCONTO;
     std::cout << finalprice << " reais" << '\n';
 
-//NAMESPACE
-    std::cout << age << '\n'; // 16
+    // NAMESPACE
+    std::cout << age << '\n';      // 16
     std::cout << one::age << '\n'; // 1
 
     return 0; // Se coloca return 0 para saber se o codigo esta dando certo, se não estiver seria "1"
 }
 
-
-int garbage(){
+int garbage()
+{
     // Garbage - Feito para funções que não realmente fazem falta / tem melhores
     std::cout << "Lixo Eletronico";
 
-    // colocar forac de garbage - 
+    // colocar forac de garbage -
 
     // typedef std::string string; //typedef - faz um "tipo" de data, utilizando outros tipos, usado para encurtar codigo, melhor usar using
 
     return 0;
-
 }
 
-int teste(){
+int teste()
+{
     using namespace one; // using namespace - coloca como padrão as variaveis do namespace one
-    using std::cout; // using std::count - coloca como padrão o std::cout, assim, podendo diminuir codigo
+    using std::cout;     // using std::count - coloca como padrão o std::cout, assim, podendo diminuir codigo
     using string = std::string;
 
-    cout << age << '\n'; 
+    cout << age << '\n';
 
     string name = "Abbot";
 
@@ -68,9 +68,38 @@ int teste(){
     return 0;
 }
 
-// int main() - é a função principal do codigo,é nescessaria para o codigo rodar, e é o "esqueleto" do codigo
-int main(){
-    // helloworld();
-    teste();
 
+void basicmath(int a, int b){
+        // a = a + b;
+        // a+= 1
+        // a++;
+
+        std::cout << "adição: " << a + b << '\n';
+
+        // a--;
+        // a-=b
+        // a = a - b;
+
+        std::cout << "subtração: " << a - b << '\n';
+
+        // a = a * b;
+        // a *= b;
+
+        std::cout << "multiplicação: " << a * b << '\n';
+
+        // a = a / b;
+        // a /= b;
+
+        std::cout << "divisão: " << a / b << '\n';
+
+        // int restante = a % b;
+
+    std::cout << "Se dividirmos "<< a << " por " << b << " teriamos " << a % b << " restantes" << '\n';
+}
+// int main() - é a função principal do codigo,é nescessaria para o codigo rodar, e é o "esqueleto" do codigo
+int main()
+{
+    helloworld();
+    // basicmath(10, 2);
+    // teste();
 }
